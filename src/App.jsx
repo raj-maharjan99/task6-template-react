67b142a5cb815e1552f88b90edaa27d57049443f
samragi99 <143216421+samragi99@users.noreply.github.com>
2023-09-09 01:15:35 -0500

    create beautiful header

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import styles from "./app.module.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,38 @@
+import "./header.css";
+const Header = () => {
+  return (
+    <header className="header_component">
+      <img
+        className="image_component"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Flag_of_South_Korea.png"
+        alt="korean flag"
+      />
+
+      <h1 className="font_component">SOUTH KOREA</h1>
+      <h1 className="font_component">NEPAL</h1>
+      <img
+        className="nepal_flag"
+        src="https://www.colonialflag.com/cdn/shop/products/nepal-flag__24265.1639690372.1280.1280.jpg?v=1673390878"
+        alt="nepal flag"
+      />
+      <nav className="nav_component">
+        <a href="#" style={{ background: "yellow" }}>
+          Home
+        </a>
+        <a href="#" style={{ background: "pink" }}>
+          {" "}
+          About
+        </a>
+        <a href="#" style={{ background: "skyblue" }}>
+          Contact
+        </a>
+        <a href="#" style={{ background: "yellowgreen" }}>
+          {" "}
+          Gallery
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
